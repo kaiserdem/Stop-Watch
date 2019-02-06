8d611b13a789004a32fef39694572a36416ee044
Yaroslav Golinskiy <kaiserdem@gmail.com>
2019-02-06 10:12:42 +0200

    First commit

---
+++ Stop Watch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Stop Watch
+//
+//  Created by Kaiserdem on 06.02.2019.
+//  Copyright © 2019 Kaiserdem. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
