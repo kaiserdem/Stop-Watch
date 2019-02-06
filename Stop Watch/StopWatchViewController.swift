75cda0967aaf8cb1c16441dc0c3f3a556a49c53f
Yaroslav Golinskiy <kaiserdem@gmail.com>
2019-02-06 16:54:29 +0200

    changed design screen of the application

--- Stop Watch/StopWatchViewController.swift
+++ Stop Watch/StopWatchViewController.swift
@@ -25,16 +25,33 @@ class StopWatchViewController: UIViewController {
     resetButton.isEnabled = false
     pauseButton.isEnabled = false
     startButton.isEnabled = true
+    
+    timerLable.layer.cornerRadius = 5.0
+    timerLable.layer.masksToBounds = true
+    
+    startButton.layer.cornerRadius = startButton.bounds.width / 2.0
+    startButton.layer.masksToBounds = true
+    
+    pauseButton.layer.cornerRadius = pauseButton.bounds.width / 2.0
+    pauseButton.layer.masksToBounds = true
+    pauseButton.alpha = 0.5
+    
+    resetButton.layer.cornerRadius = 5.0
+    resetButton.layer.masksToBounds = true
+    resetButton.alpha = 0.5
   }
   
   @IBAction func resetDidTap(_ sender: Any) {
     timer.invalidate()
     isTimerRunning = false
     counter = 0.0
-    timerLable.text = "00:00:00.0"
+    timerLable.text = "0:00:00.0"
     resetButton.isEnabled = false
     pauseButton.isEnabled = false
     startButton.isEnabled = true
+    
+    startButton.alpha = 1.0
+    pauseButton.alpha = 1.0
   }
   @IBAction func startDidTap(_ sender: Any) {
     if !isTimerRunning {
@@ -44,12 +61,20 @@ class StopWatchViewController: UIViewController {
       resetButton.isEnabled = true
       pauseButton.isEnabled = true
       startButton.isEnabled = false
+      
+      resetButton.alpha = 1.0
+      pauseButton.alpha = 1.0
+      startButton.alpha = 0.5
     }
   }
   @IBAction func pauseDidTap(_ sender: Any) {
     resetButton.isEnabled = true
     startButton.isEnabled = true
-    resetButton.isEnabled = false
+    resetButton.isEnabled = true
+    
+    startButton.alpha = 1.0
+    pauseButton.alpha = 0.5
+    resetButton.alpha = 1.0
     
     isTimerRunning = false
     timer.invalidate()
